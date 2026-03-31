e9b4b5512ee19ec37cddd616306b382a7117794b
khushang62-ui <khushang62@gmail.com>
2026-03-31 15:19:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,69 +4,38 @@ variable "aws_region" {
 }
 
 variable "ami_id" {
-  type = string
+  description = "AMI ID"
+  type        = string
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t2.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "subnet_id" {
-  type = string
+  description = "Subnet ID"
+  type        = string
 }
 
 variable "private_ip" {
-  type = string
+  description = "Private IP"
+  type        = string
 }
 
 variable "security_group_id" {
-  type = string
+  description = "Security group ID"
+  type        = string
 }
 
 variable "availability_zone" {
-  type = string
+  description = "Availability zone"
+  type        = string
 }
 
 variable "volume_size" {
-  type    = number
-  default = 70
+  description = "Root volume size"
+  type        = number
+  default     = 70
 }
-
-variable "extra_volumes" {
-  type = list(object({
-    device_name = string
-    volume_size = number
-    volume_type = string
-  }))
-  default = []
-}
-
-variable "display_name" {
-  type = string
-}
-
-variable "domain_name" {
-  type = string
-}
-
-variable "primary_nameserver" {
-  type = string
-}
-
-variable "secondary_nameserver" {
-  type = string
-}
-
-variable "instance_kms_key_id" {
-  type = string
-}
-
-variable "volume_kms_key_id" {
-  type = string
-}
-
-variable "jira" { type = string }
-variable "owner" { type = string }
-variable "pmodule" { type = string }
-variable "module" { type = string }
